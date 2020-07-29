5a275f59290aa94fd20fadd1fd32c283571ffe41
Raphael Morette <raphaelmrtt14@gmail.com>
2020-07-28 23:51:34 -0300

    Commit rotina

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria'
+
+//Desafio de jogo na página de erro
+const Pagina404 = () => (<div><h1>Amigo, acho que você digitou algo errado...</h1> </div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<BrowserRouter>
+  <Switch>
+    <Route path="/" component={Home} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+
+    <Route component={Pagina404} />
+
+  </Switch>
+</BrowserRouter>,
   document.getElementById('root')
 );
 
