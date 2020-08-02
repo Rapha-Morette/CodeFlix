772cae7d1b0500d418f76bcf3b5e3797c921cf86
Raphael Morette <raphaelmrtt14@gmail.com>
2020-08-02 20:10:04 -0300

    Ultimo commit

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,7 +1,7 @@
 import React from "react";
+import styled, { css } from 'styled-components';
 import Menu from "../menu";
 import Footer from "../Footer";
-import styled from 'styled-components';
 
 const Main = styled.main`
     background-color: var(--black);
@@ -10,14 +10,17 @@ const Main = styled.main`
     padding-top: 50px;
     padding-left: 5%;
     padding-right: 5%;
+    ${({ paddingAll }) => paddingAll && css `
+        padding: ${paddingAll};
+    `}
 `;
 
 
-function PageDefault({children}) {
+function PageDefault({ children, paddingAll }) {
     return(
         <>
             <Menu />
-             <Main>
+             <Main paddingAll={paddingAll}>
               {children}  
              </Main>
             <Footer />
